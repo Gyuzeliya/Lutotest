9e56df1eeed737d96f2f4e08d53191dd3a6beef2
Gyuzeliya <gyzeliya@gmail.com>
2020-04-04 22:00:04 +0300

    Рефакторинг.

--- main.js
+++ main.js
@@ -1,39 +1,25 @@
-// Эта функция по идее должна быть импортирована,
-// но упрощено и нужно её простейшим образом реализовать
 const serverApiRequest = url => {
-  /*simulate request*/
   return fetch(url)
     // TODO: Добавить в ответ сервера на 404 заголовок Access-Control-Allow-Origin
     .then(res => res.ok ? res.json() : Promise.reject(Error(res.statusText)))
-    .catch(e => console.log(e))
+    .catch(err => console.log(err))
 };
 
-// Можно выполнить по аналогии с serverApiRequest(), а можно лучше, см. подсказку ниже
 const sendAnalytics = (url, data) => {
-  /*sendBeacon maybe*/
   let result = navigator.sendBeacon(url, JSON.stringify(data));
-  if (result) {
-    console.log('Добавлено в очередь');
-  } else {
+  if (!result) {
     // TODO: Реализовать поддержку IE, Opera Mini
     // TODO: Изучить https://volument.com/blog/sendbeacon-is-broken
   }
 };
 
-/* Нужно:
-    1 Сделать функцию рабочей в принципе не меняя логики но доступно ES8+
-    2 Общая логика: запрос, если успех, то отправка данных в аналитику, обработка данных и их возврат
-    3 Подсветить места, где ТЗ недостаточно
-    4 Подсветить места, вероятно проблемные
-*/
 const requestData = async ({ id, param }) => {
-  // should return [null, {v: 1}, {v: 4}, null] or Error (may return array (null | {v: number})[])
   let url = '//t.syshub.ru';
-  let array = await serverApiRequest(`${url}/query/data/${id}/param/${param}`);
+  let data = await serverApiRequest(`${url}/query/data/${id}/param/${param}`);
   let result = [];
 
-  // after complete request if *not* Error call
-  if(typeof array !== 'undefined') {
+  if(typeof data !== 'undefined') {
+    // аналитика отправляется несмотря на возможное отсутствие валидных данных
     sendAnalytics(`${url}/requestDone`, {
       type: "data",
       id: id,
@@ -41,7 +27,7 @@ const requestData = async ({ id, param }) => {
     });
 
     // фильтрация массива, извлечение значения v
-    result = array.filter(item => item && item.hasOwnProperty('v')).map(item => item.v);
+    result = data.filter(item => item && item.hasOwnProperty('v')).map(item => item.v);
   }
   return result;
 };
